54938850cb8dbe4e97f4a9aba1a62afe6ed78e0d
alissontenorio <alissonal@gmail.com>
2020-01-15 18:05:49 -0300

    Creating gitignore

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ def calc_money_value_today_given_year():
     # How much var money in a given year is worth today
     money_value = float(request.args.get('money_value', None))
     given_year = int(request.args.get('year', None))
-    currency = request.args.get('currency', default="BRL")
-
+    #currency = request.args.get('currency', default="BRL")
     #if currency != "BRL":
     #    return "Currency not registered"
-
     return str(operations.calc_money_value(money_value, given_year))
 
 
